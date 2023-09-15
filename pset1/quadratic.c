7cb23dfb0773679559a820bac4454ea5e87f9dd4
Muallim718 <100230672+Muallim718@users.noreply.github.com>
2023-09-15 23:52:45 +0000

    Completed quadratic

--- pset1/quadratic.c
+++ pset1/quadratic.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 int main(){
     // Get input for a
@@ -22,8 +23,33 @@ int main(){
     } while (c < -100 || c > 100);
 
     // Compute the discriminant
-    double discriminant = sqrt(pow(b, 2) - 4 * a * c);
-    printf("This is the discriminant %f\n", discriminant);
+    float discriminant = pow(b, 2) - 4 * a * c;
 
+    float a_float = (float) a;
+    float b_float = (float) b;
+    float c_float = (float) c;
+
+    // If the discriminant is positive
+    if (discriminant > 0) {
+        float positive_root = (-1 * b + sqrt(discriminant)) / (2 * a_float);
+        float negative_root = (-1 * b - sqrt(discriminant)) / (2 * a_float);
+        printf("The solutions to the quadratic equation are as follows: \n");
+        printf("Root: %0.3f\n", positive_root);
+        printf("Root: %0.3f\n", negative_root);
+    // If the discriminant is negative
+    } else if (discriminant < 0) {
+        float real_part = (-1 * b_float) / (2 * a_float);
+        printf("Real part: %0.3f\n", real_part);
+        float imaginary_part = sqrt(abs(discriminant)) / (2 * a_float);
+        printf("The solutions to the quadratic equation are as follows: \n");
+        printf("Root: %0.3f + j%0.3f\n", real_part, imaginary_part);
+        printf("Root: %0.3f - j%0.3f\n", real_part, imaginary_part);
+    // If the discriminant is zero 
+    } else {
+        float real_part = (-1 * b_float) / (2 * a_float);
+        printf("The solutions to the quadratic equation are as follows: \n");
+        printf("Root: %0.3f, with a multiplicity of 2\n", real_part);
+    }
+    
     return 0;
 }
