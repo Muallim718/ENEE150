fb80c1fda819e93fcdfdd7809800f18901f3d653
Muallim718 <100230672+Muallim718@users.noreply.github.com>
2023-09-16 17:34:38 +0000

    Completed temperature

--- pset1/temperature.c
+++ pset1/temperature.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+void print_top_row();
+void print_celsius_spaces();
+void print_celsius_value(int celsius_temp);
+void print_fahrenheit_value(int fahrenheit_temp);
+void print_fahrenheit_spaces();
+
+int celsius_spaces = 0;
+int fahrenheit_spaces = 0;
+
 int main(){
     // Starting temperature in celsius
     float celsius_temp = 100;
@@ -8,17 +17,91 @@ int main(){
     // Initialize count variable
     int count = 0;
     // Initialize array of temperatures
-    int array_size = 20;
+    int array_size = 13;
     float temps[array_size];
-    // For loop decrementating the temperature
     while (!(celsius_temp < -20)) {
-        printf("Celsius temp: %0.3f\n", celsius_temp);
+        // Convert temperature from celsius to fahrenheit
         fahrenheit_temp = (9.0 / 5.0) * celsius_temp + 32.0;
+        // Store result in array
         temps[count++] = fahrenheit_temp;
-        printf("Fahrenheit temp: %0.3f\n", fahrenheit_temp);
+        // Decrementate the temperature
         celsius_temp = celsius_temp - 10;
     }
-    // Convert temperature from celsius to fahrenheit 
-    // Store result in array
+
+    // Reinitialize values
+    int celsius_temperature = 100;
+    count = 0;
+
     // Print table
+    int table_rows = 29;
+    int table_col = 48;
+    int space_amount = 8;
+    int top_row = 1; 
+
+    for (int i = 0; i < table_rows; i++) {
+        if (i % 2 == 0) {
+            for (int j = 0; j < table_col; j++) {
+                printf("-");
+            }
+            printf("\n");
+        } else {
+            if (i == top_row) {
+                print_top_row();
+            } else {
+                printf("| ");
+                print_celsius_value(celsius_temperature);
+                celsius_temperature -= 10;
+                print_celsius_spaces();
+                printf("| ");
+                int fahrenheit_temperature = (int) temps[count++];
+                print_fahrenheit_value(fahrenheit_temperature);
+                print_fahrenheit_spaces();
+                printf("|");
+                printf("\n");
+            }
+        }
+    }
+}
+
+void print_celsius_spaces() {
+    for (int i = 0; i < celsius_spaces; i++) {
+        printf(" ");
+    }
+}
+
+void print_celsius_value(int celsius_temp) {
+    printf("%i", celsius_temp);
+
+    if (celsius_temp >= 10 && celsius_temp <= 90) {
+        celsius_spaces = 18;
+    } else if (celsius_temp == 100 || celsius_temp < 0) {
+        celsius_spaces = 17;
+    } else if (celsius_temp == 0) {
+        celsius_spaces = 19;
+    }
+}
+
+void print_fahrenheit_spaces() {
+    for (int i = 0; i < fahrenheit_spaces; i++) {
+        printf(" ");
+    }
+}
+
+void print_fahrenheit_value(int fahrenheit_temp) {
+    printf("%i", fahrenheit_temp);
+
+    if (fahrenheit_temp > 100) {
+        fahrenheit_spaces = 20;
+    } else if (fahrenheit_temp < 100) {
+        fahrenheit_spaces = 21;
+    }
+}
+
+void print_top_row() {
+    printf("| ");
+    printf("Celsius Temperature");
+    printf(" | ");
+    printf("Fahrenheit Temperature");
+    printf(" |");
+    printf("\n");
 }
