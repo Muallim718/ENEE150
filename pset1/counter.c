35e2f9a3f3c2f3b9f8f9efb94ad270a8d5fe9c2c
Muallim718 <100230672+Muallim718@users.noreply.github.com>
2023-09-16 21:50:03 +0000

    Completed problem 4

---
+++ pset1/counter.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    // Initialize blank counter
+    int blank_counter = 0;
+    // Initialize tab counter
+    int tab_counter = 0;
+    // Initialize new line counter
+    int new_line_counter = 0;
+    // Initialize char variable
+    char c;
+    // Read input from user
+    printf("Enter text: \n");
+    while ((c = getchar()) != EOF) {
+        // Increment count variables
+        // ASCII for new line character
+        if (c == 10) {
+            new_line_counter++;
+        // ASCII for new line character
+        } else if (c == 9) {
+            tab_counter++;
+        // ASCII for space character
+        } else if (c == 32) {
+            blank_counter++;
+        }
+    }
+    printf("\n");
+    // Print count variables
+    printf("Number of blanks counted: %i\n", blank_counter);
+    printf("Number of tabs counted: %i\n", tab_counter);
+    printf("Number of new lines counted: %i\n", new_line_counter);
+
+    return 0;
+}
