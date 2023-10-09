a454a3b6b1dc2cc52132af08dffcba7c454635a8
Muallim718 <100230672+Muallim718@users.noreply.github.com>
2023-10-09 23:34:22 +0000

    Project progress

--- project1/grades.c
+++ project1/grades.c
@@ -2,6 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 
+float *compute_average(float **grades, float *highest_grades, 
+                       int num_students, int num_tests);
+
+char *determine_grades(float *average_scores, int num_students);
+
 int main(void) {
     /* Initialize name array */
     int num_students = 5;
@@ -21,24 +26,56 @@ int main(void) {
 
     /* Initialize grades array */
     int num_tests = 4;
-    int **grades;
-    grades = (int **) malloc(num_students * sizeof(int*));
+    float **grades;
+    grades = (float **) malloc(num_students * sizeof(float*));
     for (i = 0; i < num_students; i++) {
-        grades[i] = (int *) malloc(num_tests * sizeof(int));
+        grades[i] = (float *) malloc(num_tests * sizeof(float));
     }
     for (i = 0; i < num_students; i++) {
         printf("Input grades for %s\n", students[i]);
         for (j = 0; j < num_tests; j++) {
-            int grade;
-            printf("Test %i: ", j + 1);
-            scanf("%d", &grade);
+            float grade;
+            do {
+                printf("Test %i: ", j + 1);
+                scanf("%f", &grade);
+            } while (grade < 0.0 || grade > 100.0);
             grades[i][j] = grade;
         }
     }
 
     /* Initialize highest grades array */
-    int *highest_grades;
-    highest_grades = (int *) malloc(num_students * sizeof(int));
+    float *highest_grades;
+    float max_grade = 0.0;
+    highest_grades = (float *) malloc(num_students * sizeof(float));
+
+    for (i = 0; i < num_students; i++) {
+        max_grade = 0.0;
+        for (j = 0; j < num_tests; j++) {
+            if (grades[i][j] > max_grade) {
+                max_grade = grades[i][j];
+            }
+        }
+        highest_grades[i] = max_grade;
+    }
+
+    float *average_scores = compute_average(grades, highest_grades, 
+                            num_students, num_tests);
+
+    char *final_grades = determine_grades(average_scores, num_students);
+
+    for (i = 0; i < num_students; i++) {
+        printf("%0.2f\n", average_scores[i]);
+    }
+
+    for (i = 0; i < num_students; i++) {
+        printf("%c\n", final_grades[i]);
+    }
+
+    /* Free final grades array*/
+    free(final_grades);
+
+    /* Free average scores array*/
+    free(average_scores);
 
     /* Free highest grades array*/
     free(highest_grades);
@@ -55,4 +92,44 @@ int main(void) {
     }
     free(students);
     return 0;
+}
+
+float *compute_average(float **grades, float *highest_grades, 
+                       int num_students, int num_tests) {
+    int i, j;
+    float *average_scores;
+    float sum = 0;
+    float result = 0;
+    average_scores = (float *) malloc(num_students * sizeof(float));
+    for (i = 0; i < num_students; i++) {
+        sum += highest_grades[i];
+        for (j = 0; j < num_tests; j++) {
+            sum += grades[i][j];
+        }
+        result = sum / (float) (num_tests + 1);
+        average_scores[i] = result; 
+        sum = 0;
+    }
+    return average_scores;
+}
+
+char *determine_grades(float *average_scores, int num_students) {
+    int i;
+    char *final_grades;
+    final_grades = (char *) malloc(num_students * sizeof(char) + 1);
+    for (i = 0; i < num_students; i++) {
+        if (average_scores[i] >= 90 && average_scores[i] <= 100) {
+            final_grades[i] = 'A';
+        } else if (average_scores[i] >= 80 && average_scores[i] <= 89) {
+            final_grades[i] = 'B';
+        } else if (average_scores[i] >= 70 && average_scores[i] <= 79) {
+            final_grades[i] = 'C';
+        } else if (average_scores[i] >= 60 && average_scores[i] <= 69) {
+            final_grades[i] = 'D';
+        } else {
+            final_grades[i] = 'E';
+        }
+    }
+
+    return final_grades;
 }
