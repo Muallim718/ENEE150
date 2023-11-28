fa7fb6ee8439d6b6654905c5581cceb0ac01e5f3
Muallim718 <100230672+Muallim718@users.noreply.github.com>
2023-11-28 03:06:24 +0000

    Completed tree

--- project2/p_1.c
+++ project2/p_1.c
@@ -13,12 +13,15 @@ typedef struct node {
     struct node *right;
 } Node;
 
-static int AddNode(Node **root, char *input_name, char* input_num);
-static char *get_largest(Node **root);
-static char *get_smallest(Node **root);
-static int DeleteNode(Node **root, char *input_name, char* input_num);
-static int DeleteAll(Node **root);
-static void printTree(Node **root);
+int debug = 0;
+#define debug(args...) if (debug) printf(args)
+
+int AddNode(Node **root, char *input_name, char* input_num);
+Node *get_largest(Node **root);
+Node *get_smallest(Node **root);
+int DeleteNode(Node **root, char *input_name);
+int DeleteAll(Node **root);
+void printTree(Node **root);
 
 int main(void) {
     Node *root = NULL;
@@ -48,9 +51,10 @@ int main(void) {
     return 0;
 }
 
-static int AddNode(Node **root, char *input_name, char* input_num) {
+int AddNode(Node **root, char *input_name, char* input_num) {
     Node *new = NULL;
     int ret = 0;
+    int count = 0;
 
     /* Check if root was passed in null*/
     if (root) {
@@ -84,58 +88,57 @@ static int AddNode(Node **root, char *input_name, char* input_num) {
     return ret;
 }
 
-static char *get_largest(Node **root) {
-    char *ret = NULL;
+Node *get_largest(Node **root) {
+    Node *temp = NULL;
 
     if (root && *root) {
         if ((*root) -> right) {
-            ret = get_largest(&((*root) -> right));
+            temp = get_largest(&((*root) -> right));
         } else {
-            ret = malloc(strlen((*root) -> name) + 1);
-            if (ret) {
-                strcpy(ret, (*root) -> name);
-            }
+            temp = *root;
         }
     }
-    return ret;
+    return temp;
 }
 
-static char *get_smallest(Node **root) {
-    char *ret = NULL;
+Node *get_smallest(Node **root) {
+    Node *temp = NULL;
 
     if (root && *root) {
         if ((*root) -> left) {
-            ret = get_smallest(&((*root) -> left));
+            temp = get_smallest(&((*root) -> left));
         } else {
-            ret = malloc(strlen((*root) -> name) + 1);
-            if (ret) {
-                strcpy(ret, (*root) -> name);
-            }
+            temp = *root;
         }
     }
-    return ret;
+    return temp;
 }
 
 
-static int DeleteNode(Node **root, char *input_name, char* input_num) {
+int DeleteNode(Node **root, char *input_name) {
     int ret = 0;
+    Node *temp = 0;
+    int count = 0;
 
     if (root) {
         if (*root) {
             if (strcmp((*root) -> name, input_name) > 0) {
-                ret = DeleteNode(&((*root) -> left), input_name, input_num);
+                ret = DeleteNode(&((*root) -> left), input_name);
             } else if (strcmp((*root) -> name, input_name) < 0) {
-                ret = DeleteNode(&((*root) -> right), input_name, input_num);
+                ret = DeleteNode(&((*root) -> right), input_name);
             } else {
-                if ((*root) -> left) {
-                    (*root) -> name = get_largest(&((*root) -> left));
-                    ret = DeleteNode(&((*root) -> left), input_name, input_num);
-                } else if ((*root) -> right) {
-                    (*root) -> name = get_smallest(&((*root) -> right));
-                    ret = DeleteNode(&((*root) -> right), input_name, input_num);
+                free((*root) -> name);
+                free((*root) -> phone_num);
+                if ((*root) -> left || (*root) -> right) {
+                    temp = (*root) -> left ? get_largest(&((*root) -> left)) : get_smallest(&((*root) -> right));
+                    (*root) -> name = malloc(strlen(temp -> name) + 1);
+                    strcpy((*root) -> name, temp -> name);
+                    (*root) -> name[strlen(temp -> name)] = '\0';
+                    (*root) -> phone_num = malloc(strlen(temp -> phone_num) + 1);
+                    strcpy((*root) -> phone_num, temp -> phone_num);
+                    (*root) -> phone_num[strlen(temp -> phone_num)] = '\0';
+                    ret = DeleteNode(((*root) -> left ? &((*root) -> left) : &((*root)) -> right), (*root) -> name);
                 } else {
-                    free((*root) -> name);
-                    free((*root) -> phone_num);
                     free((*root));
                     *root = NULL;
                     ret = 1;
@@ -147,15 +150,18 @@ static int DeleteNode(Node **root, char *input_name, char* input_num) {
     return ret;
 }
 
-static int DeleteAll(Node **root) {
-    if (root && *root) {
-        DeleteNode(root, (*root)->name, (*root)->phone_num);
-        DeleteAll(root);
+int DeleteAll(Node **root) {
+    int ret = 0;
+
+    if (root) {
+        while (*root) {
+            ret = DeleteNode(root, (*root) -> name);
+        }
     }
     return 0;
 }
 
-static void printTree(Node **root) {
+void printTree(Node **root) {
     if (root) {
         if (*root) {
             printTree(&((*root) -> left));
